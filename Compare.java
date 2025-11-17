e66bb62cae4d222ad42363d657368ee7760ce3df
yashbardhancs24-glitch <yash.bardhan_cs24@gla.ac.in>
2025-11-17 13:31:08 +0530

    Delete Compare.java

--- Compare.java
+++
@@ -1,9 +0,0 @@
-class Compare{
-    static <T> boolean isEqual(T a,T b){
-        return a.equals(b);
-    }
-    public static void main(String[] x){
-        System.out.println(isEqual(5,5));
-        System.out.println(isEqual("a","b"));
-    }
-}
